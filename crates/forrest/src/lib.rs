48b994444a9e27c8efe309ce1e8a99b7ac9766bf
Nathaniel McCallum <nathaniel@profian.com>
2022-08-25 18:52:31 -0400

    feat[forrest]: genericize Digestable
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- crates/forrest/src/lib.rs
+++ crates/forrest/src/lib.rs
@@ -3,149 +3,149 @@ pub mod map;
 
 use digest::{Digest, Output};
 
-pub trait Digestable {
-    fn update(&self, digest: &mut impl Digest);
+pub trait Digestable<D: Digest> {
+    fn update(&self, digest: &mut D);
 
-    fn digest<D: Digest>(&self) -> Output<D> {
+    fn digest(&self) -> Output<D> {
         let mut digest = D::new();
         self.update(&mut digest);
         digest.finalize()
     }
 }
 
-impl Digestable for Vec<u8> {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for Vec<u8> {
+    fn update(&self, digest: &mut D) {
         digest.update((self.len() as u64).to_le_bytes());
         digest.update(self);
     }
 }
 
-impl Digestable for [u8] {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for [u8] {
+    fn update(&self, digest: &mut D) {
         digest.update((self.len() as u64).to_le_bytes());
         digest.update(self);
     }
 }
 
-impl Digestable for String {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for String {
+    fn update(&self, digest: &mut D) {
         digest.update((self.len() as u64).to_le_bytes());
         digest.update(self.as_bytes());
     }
 }
 
-impl Digestable for &str {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for &str {
+    fn update(&self, digest: &mut D) {
         digest.update((self.len() as u64).to_le_bytes());
         digest.update(self.as_bytes());
     }
 }
 
-impl Digestable for u8 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for u8 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for u16 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for u16 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for u32 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for u32 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for u64 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for u64 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for u128 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for u128 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for i8 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for i8 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for i16 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for i16 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for i32 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for i32 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for i64 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for i64 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for i128 {
-    fn update(&self, digest: &mut impl Digest) {
+impl<D: Digest> Digestable<D> for i128 {
+    fn update(&self, digest: &mut D) {
         digest.update(self.to_le_bytes());
     }
 }
 
-impl Digestable for () {
-    fn update(&self, _: &mut impl Digest) {}
+impl<D: Digest> Digestable<D> for () {
+    fn update(&self, _: &mut D) {}
 }
 
-impl<T0> Digestable for (T0,)
+impl<D: Digest, T0> Digestable<D> for (T0,)
 where
-    T0: Digestable,
+    T0: Digestable<D>,
 {
-    fn update(&self, digest: &mut impl Digest) {
+    fn update(&self, digest: &mut D) {
         self.0.update(digest);
     }
 }
 
-impl<T0, T1> Digestable for (T0, T1)
+impl<D: Digest, T0, T1> Digestable<D> for (T0, T1)
 where
-    T0: Digestable,
-    T1: Digestable,
+    T0: Digestable<D>,
+    T1: Digestable<D>,
 {
-    fn update(&self, digest: &mut impl Digest) {
+    fn update(&self, digest: &mut D) {
         self.0.update(digest);
         self.1.update(digest);
     }
 }
 
-impl<T0, T1, T2> Digestable for (T0, T1, T2)
+impl<D: Digest, T0, T1, T2> Digestable<D> for (T0, T1, T2)
 where
-    T0: Digestable,
-    T1: Digestable,
-    T2: Digestable,
+    T0: Digestable<D>,
+    T1: Digestable<D>,
+    T2: Digestable<D>,
 {
-    fn update(&self, digest: &mut impl Digest) {
+    fn update(&self, digest: &mut D) {
         self.0.update(digest);
         self.1.update(digest);
         self.2.update(digest);
     }
 }
 
-impl<T0, T1, T2, T3> Digestable for (T0, T1, T2, T3)
+impl<D: Digest, T0, T1, T2, T3> Digestable<D> for (T0, T1, T2, T3)
 where
-    T0: Digestable,
-    T1: Digestable,
-    T2: Digestable,
-    T3: Digestable,
+    T0: Digestable<D>,
+    T1: Digestable<D>,
+    T2: Digestable<D>,
+    T3: Digestable<D>,
 {
-    fn update(&self, digest: &mut impl Digest) {
+    fn update(&self, digest: &mut D) {
         self.0.update(digest);
         self.1.update(digest);
         self.2.update(digest);
