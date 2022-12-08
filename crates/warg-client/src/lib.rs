5accb143f8d62befd601890d73060b106bdddedd
Kyle Brown <kyleb@liquidrocketry.com>
2022-12-08 10:09:35 -0500

    feat[warg-client]: skeleton of client

--- crates/warg-client/src/lib.rs
+++ crates/warg-client/src/lib.rs
@@ -1,58 +1,114 @@
-use std::collections::HashMap;
+use semver::Version;
+use url::Url;
 
-struct Client<Cache> {
-    cache: Cache
+/// A client for interacting with a registry
+#[allow(dead_code)]
+pub struct Client<RegistryCache, ValidationCache> {
+    registry: DomainName,
+    known_roots: Vec<Root>,
+    registry_cache: RegistryCache,
+    validation_cache: ValidationCache,
 }
 
-struct Domain(String);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DomainName(String);
 
-impl Client {
-    pub fn new(primary_registry: Domain) -> Self {
-        todo!()
+#[allow(dead_code)]
+pub struct Root {
+    bytes: Vec<u8>
+}
+
+pub trait Cache {
+    /// Insert a value into the clients cache.
+    fn insert(&mut self, key: Vec<u8>, value: Vec<u8>);
+
+    /// Lookup a previously inserted cache value.
+    fn lookup(&self, key: &[u8]) -> Option<Vec<u8>>;
+}
+
+impl<RegistryCache, ValidationCache> Client<RegistryCache, ValidationCache>
+where
+    RegistryCache: Cache,
+    ValidationCache: Cache
+{
+    /// Create a new client with no previous state
+    pub fn new(registry: DomainName, registry_cache: RegistryCache, validation_cache: ValidationCache) -> Self {
+        Self {
+            registry,
+            known_roots: Vec::new(),
+            registry_cache,
+            validation_cache
+        }
     }
 
-    pub fn from_state(primary_registry: Domain, serialized_state: Vec<u8>) -> Self {
-        todo!()
+    /// Check what registry is being used
+    pub fn registry(&self) -> DomainName {
+        self.registry.clone()
     }
 
+    /// Serialize the client state
     pub fn to_state(&self) -> Vec<u8> {
-        todo!()
+        unimplemented!()
     }
 
-    pub fn query(&mut self, constraints: VersionConstraints) -> VersionConstraints {
-        todo!()
+    /// Construct a registry client with prior state
+    pub fn from_state(registry: DomainName, state: Vec<u8>, registry_cache: RegistryCache, validation_cache: ValidationCache) -> Self {
+        drop(state);
+        Self {
+            registry,
+            known_roots: Vec::new(),
+            registry_cache,
+            validation_cache
+        };
+        todo!("Deserialize known roots from state");
     }
 
-    pub fn lookup(&mut self, version_spec: VersionSpec) -> Location {
-        todo!()
+    /// Retrieve the current valid root
+    pub fn root(&self) -> Option<Root> {
+        unimplemented!()
+    }
+
+    /// Return the information for a package
+    pub fn package_info(&mut self, root: Root, package: String) -> PackageInfo {
+        drop(root);
+        drop(package);
+        unimplemented!()
     }
 }
 
-struct VersionConstraints {
-    packages: HashMap<String, VersionRequirement>
+/// Info about a package at a point in time
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PackageInfo {
+    pub name: String,
+    /// What kind of package this is
+    pub package_type: PackageType,
+    /// The available releases of the package.
+    /// Does not include yanked releases.
+    pub releases: Vec<ReleaseInfo>
 }
 
-enum VersionRequirement {
-    LessThan(Version),
-    GreaterThan(Version)
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PackageType {
+    Component,
+    Module,
+    Interface,
+    World
 }
 
-struct Version {
-    major: u32,
-    minor: u32,
-    patch: u32
+/// Info about an available package release
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ReleaseInfo {
+    /// The version of the release
+    pub version: Version,
+    /// The release content digest
+    pub digest: Vec<u8>,
+    /// The known locations where the content may be found
+    pub known_locations: Vec<Location>
 }
 
-struct VersionSet {
-    packages: HashMap<String, VersionSpec>
-}
-
-struct VersionSpec {
-    package: String,
-    version: Version,
-    digest: String
-}
-
-enum Location {
-    HTTP(String)
+/// A way of retrieving package contents
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum Location {
+    HTTP(Url)
 }
