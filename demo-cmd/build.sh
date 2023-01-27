3b45948fdf370e963ad4fa9bde79448af9cf37f1
Lann Martin <lann.martin@fermyon.com>
2023-01-27 16:14:57 -0500

    Add demo-cmd

---
+++ demo-cmd/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash -e
+
+rustup target list --installed | grep -q wasm32-wasi \
+    || (echo 'Please `rustup target add wasm32-wasi`' && exit 1)
+
+set -x
+
+cargo build --target wasm32-wasi --release
+cp target/wasm32-wasi/release/warg-demo.wasm warg-demo-1.0.0.wasm
+
+cargo build --target wasm32-wasi --release --features evil
+cp target/wasm32-wasi/release/warg-demo.wasm warg-demo-1.0.1.wasm
