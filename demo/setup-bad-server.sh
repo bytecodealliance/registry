d875b169d486f545da1f613921738551fe1f36aa
Kyle Brown <kyleb@liquidrocketry.com>
2023-02-09 11:47:41 -0500

    chore[warg]: More demo cleanup

--- demo/setup-bad-server.sh
+++
@@ -1,8 +0,0 @@
-rm -r .server-content
-mkdir .server-content
-
-alias demo_server=./bad-server
-
-export WARG_DEMO_OPERATOR_KEY="ecdsa-p256:I+UlDo0HxyBBFeelhPPWmD+LnklOpqZDkrFP5VduASk="
-
-demo_server --content-dir .server-content
