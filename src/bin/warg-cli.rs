7cbc2e21c38f87b70798751337586806244e6d7d
Peter Huene <peter@huene.dev>
2023-03-02 14:12:04 -0800

    Add back guidance for client errors.
    This commit adds back the previous guidance for client errors.

--- src/bin/warg-cli.rs
+++ src/bin/warg-cli.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use std::process::exit;
 use warg_cli::commands::{InitCommand, InstallCommand, PublishCommand, RunCommand, UpdateCommand};
+use warg_client::ClientError;
 
 fn version() -> &'static str {
     option_env!("CARGO_VERSION_INFO").unwrap_or(env!("CARGO_PKG_VERSION"))
@@ -34,9 +35,57 @@ async fn main() -> Result<()> {
         WargCli::Publish(cmd) => cmd.exec().await,
         WargCli::Run(cmd) => cmd.exec().await,
     } {
-        eprintln!("error: {e:?}");
+        if let Some(e) = e.downcast_ref::<ClientError>() {
+            describe_client_error(e);
+        } else {
+            eprintln!("error: {e:?}");
+        }
         exit(1);
     }
 
     Ok(())
+}
+
+fn describe_client_error(e: &ClientError) {
+    match e {
+        ClientError::RegistryNotSet => {
+            eprintln!("error: registry was not initialized; use the `init` command to get started");
+        }
+        ClientError::AlreadyPublishing => {
+            eprintln!("error: a publish operation is already in progress");
+            eprintln!(
+                "use `publish submit` or `publish abort` to resolve the current publish operation"
+            );
+        }
+        ClientError::NotPublishing => {
+            eprintln!("error: there is no pending publish operation; use `publish start` to begin publishing");
+        }
+        ClientError::InitAlreadyExistingPackage => {
+            eprintln!("error: selected package name already exists; choose a different name for creating a new package");
+        }
+        ClientError::PublishToNonExistingPackage => {
+            eprintln!("error: selected package does not exist; create the package with `publish start --init`");
+        }
+        ClientError::NeededContentNotFound { digest } => {
+            eprintln!("error: content needed by the current publish operation was not found");
+            eprintln!("content with digest `{digest}` not present in contents directory");
+            eprintln!("this indicates that the directory has been modified or that a previous");
+            eprintln!("`publish release` command was interrupted.");
+        }
+        ClientError::RequestedPackageOmitted { package } => {
+            eprintln!(
+                "note: the registry did not provide the requested data about package `{package}`"
+            );
+        }
+        ClientError::PackageValidationError { package, inner } => {
+            eprintln!("error: the log for package `{package}` is invalid / corrupt: {inner:?}");
+        }
+        ClientError::PackageLogEmpty => {
+            eprintln!("error: the package could be empty or the registry could be lying.");
+            eprintln!("see issue https://github.com/bytecodealliance/registry/issues/66");
+        }
+        ClientError::OtherError(e) => {
+            eprintln!("error: {:?}", e);
+        }
+    }
 }
