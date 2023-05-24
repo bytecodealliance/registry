9d9e4b17f76b3a88355d5ea5f890555281e626c4
Peter Huene <phuene@fastly.com>
2023-05-24 11:37:03 -0700

    Prefix all environment variables used by the server with `WARG_`. (#120)
    This commit prefixes all environment variables respected in the server
    CLI to use a `WARG_` prefix.
    
    Also does the same for an environment variable used in Postgres tests.

--- demo/setup-server.sh
+++ demo/setup-server.sh
@@ -2,4 +2,4 @@ rm -rf .server-content
 mkdir .server-content
 
 alias warg-server=../target/debug/warg-server
-OPERATOR_KEY="ecdsa-p256:I+UlDo0HxyBBFeelhPPWmD+LnklOpqZDkrFP5VduASk=" warg-server --content-dir .server-content
+WARG_OPERATOR_KEY="ecdsa-p256:I+UlDo0HxyBBFeelhPPWmD+LnklOpqZDkrFP5VduASk=" warg-server --content-dir .server-content
