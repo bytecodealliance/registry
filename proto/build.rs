98a76ea08f1cb5b4b5f2e95477a5e1af60a91a56
Lann Martin <lann.martin@fermyon.com>
2023-06-28 15:50:13 -0400

    Replace protoc dependency with protox

--- proto/build.rs
+++ proto/build.rs
@@ -1,62 +1,30 @@
-use regex::Regex;
-use std::{env, io::Result, path::PathBuf, process::Command};
+use prost::Message;
 
-fn main() -> Result<()> {
-    verify_protoc_version(15, 0);
-    let warg_proto = PathBuf::from("warg/protocol/warg.proto");
-    let proofs_proto = PathBuf::from("warg/transparency/proofs.proto");
-    let proto_files = vec![warg_proto, proofs_proto];
-    let root = PathBuf::from("../../proto");
+fn main() -> anyhow::Result<()> {
+    let proto_files = &["warg/protocol/warg.proto", "warg/transparency/proofs.proto"];
 
     // Tell cargo to recompile if any of these proto files are changed
-    for proto_file in &proto_files {
-        println!("cargo:rerun-if-changed={}", proto_file.display());
+    for proto_file in proto_files {
+        println!("cargo:rerun-if-changed={proto_file}");
     }
 
-    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("proto_descriptor.bin");
+    let file_descriptor_set = protox::Compiler::new(["."])?
+        .include_source_info(true)
+        .include_imports(true)
+        .open_files(proto_files)?
+        .file_descriptor_set();
+
+    let file_descriptor_set_bytes = file_descriptor_set.encode_to_vec();
 
     prost_build::Config::new()
-        // Save descriptors to file
-        .file_descriptor_set_path(&descriptor_path)
         // Override prost-types with pbjson-types
         .compile_well_known_types()
         .extern_path(".google.protobuf", "::pbjson_types")
-        // Generate prost structs
-        .compile_protos(&proto_files, &[root])?;
+        .compile_fds(file_descriptor_set)?;
 
-    let descriptor_set = std::fs::read(descriptor_path)?;
     pbjson_build::Builder::new()
-        .register_descriptors(&descriptor_set)?
+        .register_descriptors(&file_descriptor_set_bytes)?
         .build(&[".warg.protocol", ".warg.transparency"])?;
 
     Ok(())
-}
-
-// NB: protoc changed versioning schemes with v3.20 (removed major version), see https://protobuf.dev/support/version-support/
-// This means that after 3.20.x came 21.x - the major and minor version arguments here refer to the new scheme and would have previously been minor and patch versions respectively.
-fn verify_protoc_version(min_major: u32, min_minor: u32) {
-    let protoc_path = prost_build::protoc_from_env();
-    let protoc_version_output = Command::new(protoc_path)
-        .args(["--version"])
-        .output()
-        .unwrap();
-    let protoc_version = String::from_utf8(protoc_version_output.stdout).unwrap();
-
-    // based on semver.org's recommended regex, modified for lib name and optional patch capture group to accomodate libprotoc's versioning scheme
-    let re = Regex::new(r"^libprotoc (?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.?(?P<patch>0|[1-9]\d*)?(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$").unwrap();
-    let caps = re.captures(protoc_version.trim()).unwrap();
-    let major = caps.name("major").unwrap().as_str().parse::<u32>().unwrap();
-    let minor = caps.name("minor").unwrap().as_str().parse::<u32>().unwrap();
-    let version_match = if let Some(patch) = caps.name("patch") {
-        let patch = patch.as_str().parse::<u32>().unwrap();
-        major > 3 || (major == 3 && minor > min_major && patch > min_minor)
-    } else {
-        major > min_major || (major == min_major && minor > min_minor)
-    };
-
-    if !version_match {
-        panic!(
-                "Building this crate requires a version of protoc (libprotoc) >={min_major}.{min_minor}, found: {protoc_version}\nPlease install a suitable version of protobuf (see https://github.com/protocolbuffers/protobuf for downloads and instructions as well as https://protobuf.dev/support/version-support/ for info on protobuf's versioning schema)"
-            )
-    }
 }
