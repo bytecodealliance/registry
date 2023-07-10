7796c962509e247d3f8d9c05a107f187e6e5278e
Lann Martin <lann.martin@fermyon.com>
2023-07-10 14:28:24 -0400

    Add `warg key id` subcommand

--- src/commands/key.rs
+++ src/commands/key.rs
@@ -1,8 +1,8 @@
-use crate::keyring::{delete_signing_key, get_signing_key_entry, set_signing_key};
+use crate::keyring::{delete_signing_key, get_signing_key, get_signing_key_entry, set_signing_key};
 use anyhow::{bail, Context, Result};
 use clap::{Args, Subcommand};
 use dialoguer::{theme::ColorfulTheme, Confirm};
-use keyring::Error as KeyringError;
+use keyring::{Entry, Error as KeyringError};
 use p256::ecdsa::SigningKey;
 use rand_core::OsRng;
 use warg_crypto::signing::PrivateKey;
@@ -22,6 +22,7 @@ impl KeyCommand {
             KeySubcommand::New(cmd) => cmd.exec().await,
             KeySubcommand::Set(cmd) => cmd.exec().await,
             KeySubcommand::Delete(cmd) => cmd.exec().await,
+            KeySubcommand::Id(cmd) => cmd.exec().await,
         }
     }
 }
@@ -31,27 +32,64 @@ impl KeyCommand {
 pub enum KeySubcommand {
     /// Creates a new signing key for a registry in the local keyring.
     New(KeyNewCommand),
+    /// Shows the ID of the signing key for a registry in the local keyring.
+    Id(KeyIdCommand),
     /// Sets the signing key for a registry in the local keyring.
     Set(KeySetCommand),
     /// Deletes the signing key for a registry from the local keyring.
     Delete(KeyDeleteCommand),
 }
 
-/// Creates a new signing key for a registry in the local keyring.
 #[derive(Args)]
-pub struct KeyNewCommand {
+struct KeyringEntryArgs {
     /// The name to use for the signing key.
     #[clap(long, short, value_name = "KEY_NAME", default_value = "default")]
-    pub key_name: String,
+    pub name: String,
     /// The host name of the registry to create a signing key for.
     #[clap(value_name = "HOST")]
     pub host: String,
 }
 
+impl KeyringEntryArgs {
+    fn get_entry(&self) -> Result<Entry> {
+        get_signing_key_entry(&self.host, &self.name)
+    }
+
+    fn get_key(&self) -> Result<PrivateKey> {
+        get_signing_key(&self.host, &self.name)
+    }
+
+    fn set_entry(&self, key: &PrivateKey) -> Result<()> {
+        set_signing_key(&self.host, &self.name, key)
+    }
+
+    fn delete_entry(&self) -> Result<()> {
+        delete_signing_key(&self.host, &self.name)
+    }
+}
+
+impl std::fmt::Display for KeyringEntryArgs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "`{name}` for registry `{host}`",
+            name = self.name,
+            host = self.host
+        )
+    }
+}
+
+/// Creates a new signing key for a registry in the local keyring.
+#[derive(Args)]
+pub struct KeyNewCommand {
+    #[clap(flatten)]
+    keyring_entry: KeyringEntryArgs,
+}
+
 impl KeyNewCommand {
     /// Executes the command.
     pub async fn exec(self) -> Result<()> {
-        let entry = get_signing_key_entry(&self.host, &self.key_name)?;
+        let entry = self.keyring_entry.get_entry()?;
 
         match entry.get_password() {
             Err(KeyringError::NoEntry) => {
@@ -60,22 +98,37 @@ impl KeyNewCommand {
             Ok(_) | Err(KeyringError::Ambiguous(_)) => {
                 bail!(
                     "a signing key `{name}` already exists for registry `{host}`",
-                    name = self.key_name,
-                    host = self.host
+                    name = self.keyring_entry.name,
+                    host = self.keyring_entry.host
                 );
             }
             Err(e) => {
                 bail!(
-                    "failed to get signing key `{name}` for registry `{host}`: {e}",
-                    name = self.key_name,
-                    host = self.host
+                    "failed to get signing key {entry}: {e}",
+                    entry = self.keyring_entry
                 );
             }
         }
 
         let key = SigningKey::random(&mut OsRng).into();
-        set_signing_key(&self.host, &self.key_name, &key)?;
+        self.keyring_entry.set_entry(&key)?;
 
+        Ok(())
+    }
+}
+
+/// Shows the ID of the signing key for a registry in the local keyring.
+#[derive(Args)]
+pub struct KeyIdCommand {
+    #[clap(flatten)]
+    keyring_entry: KeyringEntryArgs,
+}
+
+impl KeyIdCommand {
+    /// Executes the command.
+    pub async fn exec(self) -> Result<()> {
+        let private_key = self.keyring_entry.get_key()?;
+        println!("{}", private_key.public_key().fingerprint());
         Ok(())
     }
 }
@@ -83,12 +136,8 @@ impl KeyNewCommand {
 /// Sets the signing key for a registry in the local keyring.
 #[derive(Args)]
 pub struct KeySetCommand {
-    /// The name to use for the signing key.
-    #[clap(long, short, value_name = "KEY_NAME", default_value = "default")]
-    pub key_name: String,
-    /// The host name of the registry to set the signing key for.
-    #[clap(value_name = "HOST")]
-    pub host: String,
+    #[clap(flatten)]
+    keyring_entry: KeyringEntryArgs,
 }
 
 impl KeySetCommand {
@@ -100,12 +149,11 @@ impl KeySetCommand {
         let key =
             PrivateKey::decode(key_str).context("signing key is not in the correct format")?;
 
-        set_signing_key(&self.host, &self.key_name, &key)?;
+        self.keyring_entry.set_entry(&key)?;
 
         println!(
-            "signing key `{name}` for registry `{host}` was set successfully",
-            name = self.key_name,
-            host = self.host,
+            "signing key {keyring} was set successfully",
+            keyring = self.keyring_entry
         );
 
         Ok(())
@@ -115,37 +163,31 @@ impl KeySetCommand {
 /// Deletes the signing key for a registry from the local keyring.
 #[derive(Args)]
 pub struct KeyDeleteCommand {
-    /// The name to use for the signing key.
-    #[clap(long, short, value_name = "KEY_NAME", default_value = "default")]
-    pub key_name: String,
-    /// The host name of the registry to delete the signing key for.
-    #[clap(value_name = "HOST")]
-    pub host: String,
+    #[clap(flatten)]
+    keyring_entry: KeyringEntryArgs,
 }
 
 impl KeyDeleteCommand {
     /// Executes the command.
     pub async fn exec(self) -> Result<()> {
         let prompt = format!(
-            "are you sure you want to delete the signing key `{name}` for registry `{host}`? ",
-            name = self.key_name,
-            host = self.host
+            "are you sure you want to delete the signing key {entry}? ",
+            entry = self.keyring_entry
         );
 
         if Confirm::with_theme(&ColorfulTheme::default())
             .with_prompt(prompt)
             .interact()?
         {
-            delete_signing_key(&self.host, &self.key_name)?;
+            self.keyring_entry.delete_entry()?;
             println!(
-                "signing key `{name}` for registry `{host}` was deleted successfully",
-                name = self.key_name,
-                host = self.host,
+                "signing key {entry} was deleted successfully",
+                entry = self.keyring_entry
             );
         } else {
             println!(
                 "skipping deletion of signing key for registry `{host}`",
-                host = self.host,
+                host = self.keyring_entry.host,
             );
         }
 
