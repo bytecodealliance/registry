3f47e346ec9f3a0ba306a6e7ae1cbda0939298d2
Peter Huene <peter@huene.dev>
2023-07-12 14:24:14 -0700

    Fix postgres build with latest deps.

--- crates/server/src/datastore/postgres/schema.rs
+++ crates/server/src/datastore/postgres/schema.rs
@@ -1,7 +1,7 @@
 // @generated automatically by Diesel CLI.
 
 pub mod sql_types {
-    #[derive(diesel::sql_types::SqlType)]
+    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "record_status"))]
     pub struct RecordStatus;
 }
