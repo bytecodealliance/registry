45dc90c4a4ed8ea857a2c4862758b061b27fdc4a
Calvin Prewitt <calvin@JafLabs.com>
2023-09-27 16:14:58 -0500

    fetch logs endpoint using fetch token String (#203)
    This changes the `POST /v1/fetch/logs` endpoint to use a server
    implementation defined `fetchToken` for the last known record instead of
    the `RecordId`. It still can be the `RecordId`, but it doesn't need to
    be. In fact, the reference implementation still uses the `RecordId` for
    the `fetchToken`.
    
    The motivation here is to give server implementations greater
    flexibility on implementing the fetch logs endpoint.
    
    Also, in this PR, the Client CLI will be more resilient in handling
    records that are returned that it has already seen before.

--- crates/client/src/storage.rs
+++ crates/client/src/storage.rs
@@ -110,8 +110,11 @@ pub struct OperatorInfo {
     #[serde(default)]
     pub state: operator::LogState,
     /// The registry log index of the most recent record
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub head_registry_index: Option<RegistryIndex>,
+    /// The fetch token for the most recent record
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub head_fetch_token: Option<String>,
 }
 
 /// Represents information about a registry package.
@@ -127,8 +130,11 @@ pub struct PackageInfo {
     #[serde(default)]
     pub state: package::LogState,
     /// The registry log index of the most recent record
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub head_registry_index: Option<RegistryIndex>,
+    /// The fetch token for the most recent record
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub head_fetch_token: Option<String>,
 }
 
 impl PackageInfo {
@@ -139,6 +145,7 @@ impl PackageInfo {
             checkpoint: None,
             state: package::LogState::default(),
             head_registry_index: None,
+            head_fetch_token: None,
         }
     }
 }
